e3456bf9bafd58a792883a3ff6eb811a45059ce6
enforcer007 <akhilgodvsdemon@gmail.com>
2020-06-15 11:31:40 +0530

    Speeds up Perfomance

--- src/ArithematicAnalysis/Bisection.jl
+++ src/ArithematicAnalysis/Bisection.jl
@@ -4,7 +4,7 @@ julia> floor(ArithematicAnalysis.bisection(x->x^4-4x^3+x+4,0,3);digits=2)
 1.23
 ```
 """
-function bisection(custom_func,starting::T,ending::K) where {T<:Real,K<:Real}
+function bisection(custom_func,starting::Real,ending::Real)
     if custom_func(starting)==0
         return starting
     elseif custom_func(ending)==0
@@ -26,22 +26,22 @@ function bisection(custom_func,starting::T,ending::K) where {T<:Real,K<:Real}
     end
 end
 
-function _get_mid_point(x::T, y::K) where {T<:Real,K<:Real}
+function _get_mid_point(x::Real, y::Real)
     return x+(y-x)/2
 end
 
-function _root_evaluate(x::T) where {T<:Real}
+function _root_evaluate(x::Real)
     value = custom_func(x)
     abs_value = abs(value)
     return abs_value<10^-7
 end
 
-function _root_finder(x::T, y::K) where {T<:Real,K<:Real}
+function _root_finder(x::Real, y::Real)
     x,y = custom_func(x), custom_func(y)
     return x * y < 0
 end
 
 # Should be Monotonic Function
-function custom_func(x::T) where {T<:Real}
+function custom_func(x::Real)
     return x^4-4x^3+x+4
 end
