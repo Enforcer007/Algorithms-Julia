d136baa358cf2bff47417b9ff74b2ebe20111ca6
enforcer007 <akhilgodvsdemon@gmail.com>
2020-06-15 11:40:29 +0530

    Sends function as args

--- src/ArithematicAnalysis/Bisection.jl
+++ src/ArithematicAnalysis/Bisection.jl
@@ -9,15 +9,15 @@ function bisection(custom_func,starting::Real,ending::Real)
         return starting
     elseif custom_func(ending)==0
         return ending
-    elseif  !_root_finder(starting,ending)
+    elseif  !_root_finder(starting,ending,custom_func)
         print("Cannot find roots between a and b")
         return Nothing
     else
         mid_point = _get_mid_point(starting,ending)
-        while !_root_evaluate(mid_point)
-            if _root_finder(starting,mid_point)
+        while !_root_evaluate(mid_point,custom_func)
+            if _root_finder(starting,mid_point,custom_func)
                 ending = mid_point
-            elseif _root_finder(mid_point,ending)
+            elseif _root_finder(mid_point,ending,custom_func)
                 starting = mid_point
             end
             mid_point = _get_mid_point(starting,ending)
@@ -30,14 +30,14 @@ function _get_mid_point(x::Real, y::Real)
     return x+(y-x)/2
 end
 
-function _root_evaluate(x::Real)
-    value = custom_func(x)
+function _root_evaluate(x::Real,func)
+    value = func(x)
     abs_value = abs(value)
     return abs_value<10^-7
 end
 
-function _root_finder(x::Real, y::Real)
-    x,y = custom_func(x), custom_func(y)
+function _root_finder(x::Real, y::Real,func)
+    x,y = func(x), func(y)
     return x * y < 0
 end
 
